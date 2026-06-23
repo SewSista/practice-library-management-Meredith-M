1e97bde9647d4fff364a12535cb8a23fa9c92e18
Clover <sewsistacomo@gmail.com>
2026-06-23 16:41:31 -0500

    library program written with group

--- project-task.js
+++ project-task.js
@@ -12,15 +12,98 @@ Tasks
 1. Step 1: Create a Base Class
     ○ Define a class LibraryItem to represent shared properties
     (title, id, isAvailable) and methods (e.g., checkOut() and returnItem()).
-2. Step 2: Extend the Base Class
-    ○ Create child classes Book, DVD, and Magazine that inherit from LibraryItem.
-    ○ Add unique properties and methods for each child class:
-        ■ Book: Add properties like author and genre.
-        ■ DVD: Add properties like director and duration.
-        ■ Magazine: Add properties like issueNumber and publisher.
-3. Step 3: Instantiate Objects
-    ○ Create instances of each class and test the shared and unique methods.
-4. Step 4: Test the Inheritance
-    ○ Use inherited methods like checkOut() and returnItem() to manage the availability of items.
-    ○ Test accessing and displaying unique properties of each child class.
 */
+class LibraryItem {
+    constructor(title,id,isAvailable = true){
+        this.title = title;
+        this.id = id;
+        this.isAvailable = isAvailable;
+    }
+
+    checkout(){
+        if(this.isAvailable){
+            return `${this.title} was available and now you have it!`;
+            this.isAvailable = false;
+        }
+        else{
+            return `${this.title} is not present to take!`;
+        }
+    }
+
+    returnItem(){
+        this.isAvailable === true;
+        return `${this.title} has just been returned.`;
+    }
+}
+/*
+2. Step 2: Extend the Base Class
+○ Create child classes Book, DVD, and Magazine that inherit from
+LibraryItem.
+○ Add unique properties and methods for each child class:
+■ Book: Add properties like author and genre.
+■ DVD: Add properties like director and duration.
+■ Magazine: Add properties like issueNumber and publisher.*/
+
+class book extends LibraryItem{ //bookmark method?
+    constructor(title, id, isAvailable, author, genre, pages = 0, bookmark = 0){
+        super(title,id,isAvailable);
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+        this.bookmark = bookmark;
+    }
+
+    readBookmark(number){
+    this.bookmark = number;
+    return `You have now read through page ${this.bookmark} out of ${this.pages}.`;
+    }
+}
+
+class dvd extends LibraryItem{  //time stamp method? deleted scenes?
+    constructor(title, id, isAvailable, director, duration){
+        super(title,id,isAvailable);
+        this.director = director;
+        this.duration = duration;
+    }
+}
+
+class magazine extends LibraryItem{ //respond to ad method?
+    constructor(title, id, isAvailable, issueNumber, publisher){
+        super(title,id,isAvailable);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
+}
+
+
+
+/*
+3. Step 3: Instantiate Objects
+○ Create instances of each class and test the shared and unique
+methods.
+*/
+
+let fahrenheit451 = new book("Fahrenheit 451","123456","false","Ray Bradbury", "Horror", 362);
+let theLightningThief = new dvd("The Lightning Thief", "456789", "true", "Steven Spielberg", "108");
+let rollingStone = new magazine("Rolling Stone Magazine", "147852", "false", "98", "Penske Media Corp.");
+
+
+
+
+
+/*
+4. Step 4: Test the Inheritance
+○ Use inherited methods like checkOut() and returnItem() to
+manage the availability of items.
+○ Test accessing and displaying unique properties of each child
+class.*/
+
+console.log(fahrenheit451.readBookmark(150));
+
+
+
+
+console.log(fahrenheit451.checkout());
+console.log(theLightningThief.checkout());
+console.log(theLightningThief.returnItem());
+console.log(rollingStone.checkout());
